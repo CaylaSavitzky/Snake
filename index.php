e3ebb5376adcfe47ddc39286ae8ae9f6accea121
CaylaSavitzky <CaySavitzky@gmail.com>
2018-09-24 16:19:43 -0400

    test

--- index.php
+++ index.php
@@ -5,10 +5,10 @@
 
           <?php 
       if ( have_posts() ) : while ( have_posts() ) : the_post();
-    
+        
         get_template_part( 'content', get_post_format() );
-  
-      endwhile; endif; 
+        
+      endwhile(); endif(); 
       ?>
 
           <nav>
